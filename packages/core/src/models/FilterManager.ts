88419fea0a36b84af02b97229d78896af42c3beb
André Sarandeses <acamposarandeses@gmail.com>
2026-03-11 21:04:06 +0000

    Implement FilterManager class for managing filter operations

---
+++ packages/core/src/models/FilterManager.ts
@@ -0,0 +1,66 @@
+import { FilterI } from "./schema";
+
+export class FilterManager {
+    private filters: FilterI[] = [];
+
+    pushFilter(filter: FilterI) {
+        this.filters.push(filter);
+    }
+
+    unshiftFilter(filter: FilterI) {
+        this.filters.unshift(filter);
+    }
+
+    addFilter(filter: FilterI, at_front: boolean = false) {
+        const new_filters = at_front
+            ? [filter, ...this.filters]
+            : [...this.filters, filter];
+
+        this.filters = new_filters;
+    }
+
+    getFilter(idx: number): FilterI | undefined {
+        if (idx < 0 || idx >= this.filters.length) {
+            return undefined;
+        }
+        return this.filters[idx];
+    }
+
+    getFilters(): FilterI[] {
+        return this.filters;
+    }
+
+    setFilters(filters: FilterI[]) {
+        this.filters = filters;
+    }
+
+    moveFilterDown(idx: number) {
+        if (idx < 0 || idx >= this.filters.length - 1) {
+            return;
+        }
+        const filters = this.filters;
+        [filters[idx], filters[idx + 1]] = [filters[idx + 1], filters[idx]];
+    }
+
+    moveFilterUp(idx: number) {
+        if (idx <= 0 || idx >= this.filters.length) {
+            return;
+        }
+        const filters = this.filters;
+        [filters[idx], filters[idx - 1]] = [filters[idx - 1], filters[idx]];
+    }
+
+    deleteFilter(idx: number) {
+        if (idx < 0 || idx >= this.filters.length) {
+            return;
+        }
+        const filters = this.filters;
+        filters.splice(idx, 1);
+    }
+
+    copy(): FilterManager {
+        const fm_copy = new FilterManager();
+        fm_copy.setFilters([...this.filters]);
+        return fm_copy;
+    }
+}
