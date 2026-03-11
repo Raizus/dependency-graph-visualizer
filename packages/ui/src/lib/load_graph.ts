b8675e7904bfc852520253f47f7d82b2ac89a61a
André Sarandeses <acamposarandeses@gmail.com>
2026-03-11 18:23:02 +0000

    Add TODO comment for validation in loadPublicJson function

--- packages/ui/src/lib/load_graph.ts
+++ packages/ui/src/lib/load_graph.ts
@@ -2,5 +2,6 @@ import type { StateJSON } from "@dep-graph-vis/core";
 
 export async function loadPublicJson(path: string): Promise<StateJSON> {
     const response = await fetch(path);
+    // TODO: validation
     return await response.json();
 }
